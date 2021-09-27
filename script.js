d08c42e06e99e5176d3e3ae8d6001395e6472aa9
ying <ying285@hotmail.com>
2021-09-27 07:54:32 +0200

    modify2

--- script.js
+++ script.js
@@ -63,3 +63,15 @@ const menuPage = () => {
 };
 
 menuPage();
+
+//swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
